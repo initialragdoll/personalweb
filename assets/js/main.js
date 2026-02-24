eb8a7b7870e3a7a10cfa4036c26e9c9a4a50cc36
initialragdoll <initialragdoll@gmail.com>
2026-02-24 20:30:17 +0100

    Update main.js_auto update legal info

--- assets/js/main.js
+++ assets/js/main.js
@@ -538,10 +538,15 @@ function toggleTheme() {
 }
 
 
+/* Auto update legal info */
+document.getElementById('current-year').textContent = new Date().getFullYear();
+
+
 /*
 	Origin Template Design by HTML5 UP
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+
 
 
 
